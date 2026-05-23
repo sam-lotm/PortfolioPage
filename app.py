854a4b2403a929c8b2100a15140aeb6c9f676ecc
Sam <sammacciocca@gmail.com>
2026-05-23 15:12:50 +1000

    added inter linking pages of html together

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('homepage.html')
+    return render_template('index.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
